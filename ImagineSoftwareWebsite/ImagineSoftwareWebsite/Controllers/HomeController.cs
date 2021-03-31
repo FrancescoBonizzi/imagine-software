e82bfb3591148d4ba3a7a3214458c3f08af9afd1
Francesco Bonizzi <fbonizzi.90@gmail.com>
2021-03-31 08:02:16 +0200

    Primo commit

---
+++ ImagineSoftwareWebsite/ImagineSoftwareWebsite/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using ImagineSoftwareWebsite.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ImagineSoftwareWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
