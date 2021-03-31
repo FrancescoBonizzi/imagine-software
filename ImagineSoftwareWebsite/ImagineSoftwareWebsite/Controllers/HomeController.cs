4865b4172dabcbfb94b361498008e29f154eba13
Francesco Bonizzi <fbonizzi.90@gmail.com>
2021-03-31 14:44:47 +0200

    Footer, pagine

--- ImagineSoftwareWebsite/ImagineSoftwareWebsite/Controllers/HomeController.cs
+++ ImagineSoftwareWebsite/ImagineSoftwareWebsite/Controllers/HomeController.cs
@@ -7,9 +7,31 @@ namespace ImagineSoftwareWebsite.Controllers
     public class HomeController : Controller
     {
         public IActionResult Index()
-        {
-            return View();
-        }
+            => View();
+
+        [Route("contatti")]
+        public IActionResult Contacts()
+            => View();
+
+        [Route("clienti-casestudy")]
+        public IActionResult Customers()
+            => View();
+
+        [Route("servizi-offerta")]
+        public IActionResult Services()
+            => View();
+
+        [Route("francesco-bonizzi")]
+        public IActionResult Francesco()
+            => View();
+
+        [Route("privacy")]
+        public IActionResult Privacy()
+            => View();
+
+        [Route("sitemap")]
+        public IActionResult Sitemap()
+            => View();
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
