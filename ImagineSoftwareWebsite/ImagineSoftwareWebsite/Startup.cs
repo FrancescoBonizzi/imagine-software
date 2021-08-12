bdf6efd0e724bdc68e85334e4507ad038cd0e10b
Francesco Bonizzi <fbonizzi.90@gmail.com>
2021-08-12 13:46:01 +0200

    Rimiossa parte email

--- ImagineSoftwareWebsite/ImagineSoftwareWebsite/Startup.cs
+++ ImagineSoftwareWebsite/ImagineSoftwareWebsite/Startup.cs
@@ -1,9 +1,6 @@
-using ImagineSoftwareWebsite.Email;
 using ImagineSoftwareWebsite.HttpLifecycle;
-using ImagineSoftwareWebsiteLibrary;
 using ImagineSoftwareWebsiteLibrary.Logs;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -40,8 +37,6 @@ namespace ImagineSoftwareWebsite
                 .AddNewtonsoftJson();
 
             services.AddSingleton<IMyLogger, ConsoleLogger>();
-            services.AddSingleton<Configuration>();
-            services.AddSingleton<EmailClient>();
             services.AddSingleton<RoutesInspector>();
         }
 
