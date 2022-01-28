392d587961a869d78ae4d163ff6b5290a22548e5
Francesco Bonizzi <fbonizzi.90@gmail.com>
2022-01-28 13:11:00 +0100

    All open source projects migrated to Squidex

--- ImagineSoftwareWebsite/Controllers/OpenSourceProjectsController.cs
+++ ImagineSoftwareWebsite/Controllers/OpenSourceProjectsController.cs
@@ -10,28 +10,5 @@ namespace ImagineSoftwareWebsite.Controllers
         public IActionResult Index()
             => View();
 
-        [Route(template: "open-source-projects/boolli", Name = "Boolli: interprete di espressioni booleane")]
-        public IActionResult Boolli()
-            => View();
-
-        //[Route(template: "open-source-projects/infart-game", Name = "INFART")]
-        //public IActionResult INFART()
-        //    => View();
-
-        [Route(template: "open-source-projects/restart-on-crash", Name = "RestartOnCrash")]
-        public IActionResult RestartOnCrash()
-            => View();
-
-        [Route(template: "open-source-projects/smart-scale-3000", Name = "Smart Scale 3000: la bilancia intelligente")]
-        public IActionResult SmartScale3000()
-            => View();
-
-        [Route(template: "open-source-projects/starfall-game", Name = "Starfall")]
-        public IActionResult Starfall()
-            => View();
-
-        [Route(template: "open-source-projects/rellow-game", Name = "Rellow")]
-        public IActionResult Rellow()
-            => View();
     }
 }
