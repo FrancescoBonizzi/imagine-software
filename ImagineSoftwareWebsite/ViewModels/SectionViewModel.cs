5cfc12140a7c940398d39bb7928b03e287fb98ea
Francesco Bonizzi <f.bonizzi@outlook.it>
2022-04-10 14:16:51 +0200

    Removed old website

--- ImagineSoftwareWebsite/ViewModels/SectionViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ImagineSoftwareWebsite.ViewModels
-{
-    public class SectionViewModel
-    {
-        public string Title { get; set; }
-        public string Body { get; set; }
-    }
-}
